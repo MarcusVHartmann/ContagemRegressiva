24b75ed53d948660e4b929eb1f18afeda9270eaf
MarcusVHart <haykirengeral@gmail.com>
2025-03-23 19:23:20 -0300

    Criação de um sistema de contagem regressiva

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Preparar! . . .");
+Console.ReadKey();
+Console.WriteLine("Apontaar! . . .");
+Console.ReadKey();
+Console.WriteLine("FOGO!!!");
+Console.ReadKey();
+Console.WriteLine("ué");
+Console.ReadKey();
+Console.WriteLine("Era o alvo errado. . .");
